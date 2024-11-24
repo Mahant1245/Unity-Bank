1e1fdbb1763cd9ac22441ac673c5caa3ce2723bd
Mahant1245 <mahant1245@gmail.com>
2024-11-24 23:10:03 +0000

    about incomplete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // hamburger script
 $(document).ready(function(){
+
     const tabs=$("#tabs");
     const hamburger=$("#hamburger");
 
@@ -62,6 +63,18 @@ $(document).ready(function(){
 
 //form validation
 $(document).ready(function(){
+
+    //script for help
+    $('#helpBtn').click(function(){
+        alert("Hi,\n\nIn this form you have to write a your first and last name separately then email and subject of your email. and then include the message.\n\nFor now I have kept min len for message to be 5 character to easily test\n\n\nEnjoy!")
+    })
+    //script for reset
+    $('#resetBtn').click(function(event){
+        event.preventDefault();
+        $('#bank_form')[0].reset();
+    })
+
+    //script for submit
     $('#bank_form').on('submit',function(event){
 
         event.preventDefault();
@@ -79,7 +92,7 @@ $(document).ready(function(){
             result.text('Message submitted');
             messagebox.css('border','2px solid green');
             result.css('color','green')
-            $('#bank_form')[0].reset();//selecttor selects the form and reset the whole thing.found on google
+            $('#bank_form')[0].reset();//selecttor selects the form and reset the whole thing.found on geeksforgeeks
             setTimeout(function(){
                 result.text('');
                 messagebox.css('border','none');
@@ -87,3 +100,30 @@ $(document).ready(function(){
         }
     });
 });
+
+//about us section
+$(document).ready(function(){
+    $(".year").hover(function(){
+        var current_content=$(this).find(".year_content")
+
+        //this var gets the h and w of ew content
+        current_content.css('display','inline-block');
+        var new_height=$(this).find(".year_content").outerHeight();
+        var new_width=$(this).find(".year_content").outerWidth();
+
+        $(this).find(".year_content").animate({
+            left: '250px',
+            height: new_height,
+            width:new_width
+        }); 
+    });
+});
+
+// function(){//reset to normal
+//     $(this).animate({
+//         left: '0px',
+//         height: '100px',
+//         width:'158px'
+//     }); 
+//     $(this).find(".year_content").css('display','none');
+// }
